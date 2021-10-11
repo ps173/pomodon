3e6c85dca71ad778d8ad9e648a07ebac557ed0bc
ps173 <prathamsharma173@gmail.com>
2021-10-11 18:55:23 +0530

    Initial Commit

---
+++ src/SessionStore.js
@@ -0,0 +1,3 @@
+import { writable } from "svelte";
+
+export const sessionDetail = writable([]);
---
+++ src/routes/Stats.svelte
@@ -0,0 +1,14 @@
+<div class="stats">
+    Stats will be here soon. :)
+</div>
+
+<style>
+    .stats{
+        display: grid;
+        place-items: center;
+        color: cadetblue;
+        font-size: 1.5em;
+        height: 80%;
+        width: 100%;
+    }
+</style>
