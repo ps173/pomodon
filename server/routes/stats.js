385c0949c9bb14c61fb6e903c3d4c96a6285f66a
ps173 <prathamsharma173@gmail.com>
2021-11-02 15:21:33 +0530

    implementing put and delete with id

--- server/routes/stats.js
+++ server/routes/stats.js
@@ -2,8 +2,6 @@ const Router = require("@koa/router");
 const router = new Router();
 const userStats = require("../models/UserStats");
 
-// // TODO: Extract the get by id as a middleware
-// TODO: Add all the other id methods
 router.get("/stats/", async (ctx, next) => {
   const allStats = await userStats.find();
   ctx.body = allStats;
@@ -33,34 +31,53 @@ router.post("/stats/", async (ctx, next) => {
   await next();
 });
 
+// FIXME: REFACTOR THIS
 router.put("/stats/:user", getStatsByUserId, async (ctx, next) => {
-  ctx.body = {
-    message: `Update stats of user - ${ctx.params.user}`,
-  };
+  ctx.request.userStats.userID = ctx.request.body?.userID;
+  ctx.request.userStats.displayName = ctx.request.body?.displayName;
+  ctx.request.userStats.totalNumberOfSession =
+    ctx.request.body?.totalNumberOfSession;
+  ctx.request.userStats.totalHours = ctx.request.body?.totalHours;
+  try {
+    const updatedStats = await ctx.request.userStats.save();
+    ctx.response.body = updatedStats;
+  } catch (err) {
+    console.error(err);
+  }
   await next();
 });
 
 router.delete("/stats/", async (ctx, next) => {
   ctx.body = {
-    message: `deleted user stats`,
+    message: `deleted all user stats`,
   };
+  userStats.delte;
   await next();
 });
 
+// FIXME: GIVES 404 status code and not found in return but still deletes the user
 router.delete("/stats/:user", async (ctx, next) => {
-  ctx.body = {
-    message: `deleted stats of user - ${ctx.params.user}`,
-  };
+  userStats.findByIdAndDelete(ctx.params.user, (err, docs) => {
+    if (err) {
+      console.error(err);
+      throw err;
+    }
+    console.log("Delted : ", docs);
+    ctx.body = {
+      message: `deleted : ${docs}`,
+    };
+    ctx.status = 204;
+  });
   await next();
 });
 
+// DOCS: Middleware for getting users by id
 async function getStatsByUserId(ctx, next) {
   let stats;
   try {
     stats = await userStats.findById(ctx.params.user);
   } catch (err) {
     console.error(err);
-    stats = "user does not exists";
   }
   ctx.request.userStats = stats;
   await next();
